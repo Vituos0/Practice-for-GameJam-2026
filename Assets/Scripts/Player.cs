4a62649438b3d5e0d2e67358a106f1876b2f06bd
concatkikuc <113051123+Vituos0@users.noreply.github.com>
2026-01-19 15:45:23 +0700

    Setup Project and Creat Player Input

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Player : MonoBehaviour
+{
+
+
+    [Header("Settings")]
+
+    [SerializeField] private float ascentForece = 15f; //force to pull player up when using anti-gravity suit
+  //  [SerializeField] private float maxAscentSpeed = 10f; //max fly up speed
+  //  [SerializeField] private float heavyGravity = 5f;   //gravity when using gravity enhancer
+    [SerializeField] private float normalGravity = 1f;  //normal gravity
+   // private bool isUsingGravityEnhancer = false;
+    private bool isUsingAGSuit = false;
+
+    [Header("Components")]
+    private Rigidbody2D rb;
+
+    private void Awake()
+    {
+    }
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb= GetComponent<Rigidbody2D>();
+        rb.gravityScale= normalGravity;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+    private void FixedUpdate()
+    {
+        Movement();
+    }
+
+    private void LateUpdate()
+    {
+        
+    }
+
+    private void Movement()
+    {
+        if (isUsingAGSuit) //using Anti-gravity suit
+        {
+            rb.gravityScale = .25f;
+          //  if(rb.velocity.y < maxAscentSpeed)
+            rb.AddForce(Vector2.up * ascentForece, ForceMode2D.Force);  //player is pulled up
+
+        }
+        else   //not using gravity enhancer
+        {
+            rb.gravityScale = normalGravity;                            //give back normal gravity
+        }
+    }
+
+
+    public void MouseDownCallBack()
+    {
+        isUsingAGSuit = true;
+    }
+    public void MouseDragCallBack()
+    {
+
+    }
+    public void MouseUpCallBack()
+    {
+        isUsingAGSuit = false;
+    }
+
+}
---
+++ Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+
+
+    [Header("Data")]
+    private bool isControlling;
+
+
+    [Header("Components")]
+    [SerializeField]private Player player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ManagePlayerInput();
+    }
+    private void ManagePlayerInput()                   //Đây là hàm quản lý thao tác điều khiển đầu vào
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            isControlling= true;
+            player.MouseDownCallBack();
+        }
+
+        else if (Input.GetMouseButton(0))
+        {
+            if (isControlling)
+                player.MouseDragCallBack();
+            else
+            {   
+                isControlling= true;
+                player.MouseDownCallBack();
+            }
+        }
+        else if (Input.GetMouseButtonUp(0) && isControlling)
+        {
+            isControlling= false;
+            player.MouseUpCallBack();
+        }
+    }
+}
